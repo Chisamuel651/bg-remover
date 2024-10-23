6de559103462c42e61d505d7c51407f4c614adc2
ChiSamuel651 <chijoe0306@gmail.com>
2024-10-23 03:24:02 +0100

    remove background from image

--- server/controllers/ImageController.js
+++ server/controllers/ImageController.js
@@ -6,7 +6,22 @@ import userModel from '../models/userModel.js'
 // controller fxn to remove bg from image
 const removeBgImage = async (req, res) => {
     try {
-        
+        const {clerkId} = req.body
+        const user = await userModel.findOne({clerkId})
+
+        if(!user){
+            return res.json({success: false, message: 'User not found'})
+        }
+
+        if(user.creditBalance === 0){
+            return res.json({success: false, message: 'No credit balance', creditBalance:user.creditBalance})
+        }
+
+        const imagePath = req.file.path
+        // reading the image file
+        const imageFile = fs.createReadStream(imagePath)
+
+
     } catch (error) {
         console.log(error.message);
         res.json({ success:false, message:error.message })
--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -6,3 +6,5 @@ import authUser from '../middlewares/auth.js'
 const imageRouter = express.Router()
 
 imageRouter.post('/remove-bg', upload.single('image'), authUser,removeBgImage)
+
+export default imageRouter
